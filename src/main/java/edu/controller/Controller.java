a9c4c6617b9540fa15eaba17b8d97cccf414c38d
leehaoze <986989635@qq.com>
2018-02-04 17:20:59 +0800

    添加首屏背景的视觉差效果 首屏信息开始支持自定义

--- src/main/java/edu/controller/Controller.java
+++ src/main/java/edu/controller/Controller.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @org.springframework.stereotype.Controller
 
@@ -13,8 +15,22 @@ public class Controller {
     @ResponseBody
     public ArrayList<String> getIndexIMG(){
         ArrayList<String> img = new ArrayList<String>();
-        img.add("ShangHai01.jpg");
-        img.add("ShangHai02.jpg");
+        img.add("/IMG/ShangHai01.jpg");
+        img.add("/IMG/ShangHai02.jpg");
         return img;
     }
+
+    @RequestMapping("/getInfo")
+    @ResponseBody
+    public HashMap<String,String> getInfo(){
+        HashMap<String,String> info = new HashMap<String, String>();
+        info.put("head_pic_path","/IMG/Head-Pic.jpg");
+        info.put("bloger_name","Leehaoze");
+        info.put("quoto","Code & Life");
+        info.put("na" +
+                "me_font","Cookie");
+        info.put("quoto_font","Lato");
+        return info;
+    }
+
 }
--- web/JS/PageJS.js
+++ web/JS/PageJS.js
@@ -1,38 +1,74 @@
 $(function () {
-    loadIndexIMG();
+    IndexIMGControl.run();
+    InfoCard.run()
 });
 
-
-function loadIndexIMG() {
-    $.ajax({
-        dataType: 'json',
-        url: '/getIndexIMG.form',
-        success: function (data) {
-            IndexIMGControl.run(data);
-            console.log("AJAX SUCCESS!");
-        }
-    })
-}
-
-var count = 0;
-
 var IndexIMGControl = {
-    count : 0,
-    imgPath : undefined,
-    imgNum : 0,
-    run : function (data) {
-        this.imgPath = data;
-        this.imgNum = data.length;
-        this.animate();
-        setInterval('IndexIMGControl.animate()',10000);
+    count: 0,
+    imgPath: undefined,
+    imgNum: 0,
+    run : function () {
+        $.ajax({
+            dataType: 'json',
+            url: '/getIndexIMG.form',
+            success: function (data) {
+                IndexIMGControl.imgPath = data;
+                IndexIMGControl.imgNum = data.length;
+                IndexIMGControl.animate();
+                $('#background-pic').mousemove(function (e) {
+                    IndexIMGControl.move(e);
+                });
+                setInterval('IndexIMGControl.animate()', 10000);
+            }
+        })
     },
-    animate : function () {
-        console.log(this.imgPath[count]);
+    animate: function () {
         $('#background-pic').css({
-            'background': 'url("/IMG/' + this.imgPath[count] + '") no-repeat fixed center',
+            'background': 'url(' + this.imgPath[this.count] + ') no-repeat fixed center',
             'animation': 'img-scale 10s linear infinite forwards ',
-            'background-size':'cover'
+            'background-size': 'cover'
         });
-        count = (count + 1) % this.imgNum;
+        this.count = (this.count + 1) % this.imgNum;
+    },
+    pre_x: undefined,
+    pre_y: undefined,
+    move: function (e) {
+        var x = e.pageX;
+        var y = e.pageY;
+        if (this.pre_x != undefined && this.pre_y != undefined) {
+            $('#background-container').css({
+                'transform': 'translate(' + -(x - this.pre_x) / 100 + 'px,' + -(y - this.pre_y) / 100 + 'px)'
+            })
+        }
+        else{
+            this.pre_x = innerWidth / 2;
+            this.pre_y = innerHeight / 2;
+        }
+    }
+};
+
+var InfoCard = {
+    run : function () {
+        InfoCard.loadInfo();
+    },
+    loadInfo: function () {
+        $.ajax({
+            dataType: 'json',
+            url: '/getInfo.form',
+            success: function (data) {
+                InfoCard.writeData(data);
+            }
+        })
+    },
+    writeData: function (data) {
+        $('#head-pic').css({
+            'background': 'url(' + data['head_pic_path'] + ') no-repeat center',
+            'background-size': 'cover'
+        });
+        $('#bloger-name').text(data['bloger_name']);
+        $('#quoto').text(data['quoto']);
+        $('#bloger-name').css('font-family', data['name_font']);
+        $('#quoto').css('font-family', data['quoto_font']);
+
     }
 };
